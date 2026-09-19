13e18bab9c356b58c1852944c62ad4356e66cc83
HamidOffline <44529274+HamidOffline@users.noreply.github.com>
2026-09-19 11:42:38 +0330

    Update AboutActivity.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/AboutActivity.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/AboutActivity.kt
@@ -9,42 +9,91 @@ import com.v2ray.ang.databinding.ActivityAboutBinding
 import com.v2ray.ang.util.Utils
 
 class AboutActivity : BaseActivity() {
-    private val binding by lazy { ActivityAboutBinding.inflate(layoutInflater) }
+
+    private val binding by lazy {
+        ActivityAboutBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(binding.root)
-        setContentViewWithToolbar(binding.root, showHomeAsUp = true, title = getString(R.string.title_about))
 
+        setContentViewWithToolbar(
+            binding.root,
+            showHomeAsUp = true,
+            title = getString(R.string.title_about)
+        )
+
+        /*
+         * MojAzad GitHub source code
+         */
         binding.layoutSoureCcode.setOnClickListener {
-            Utils.openUri(this, AppConfig.APP_URL)
+            Utils.openUri(
+                this,
+                "https://github.com/HamidOffline/MojAzadV1"
+            )
         }
 
+        /*
+         * Keep current feedback link for now.
+         */
         binding.layoutFeedback.setOnClickListener {
-            Utils.openUri(this, AppConfig.APP_ISSUES_URL)
+            Utils.openUri(
+                this,
+                AppConfig.APP_ISSUES_URL
+            )
         }
 
+        /*
+         * Open-source licenses
+         */
         binding.layoutOssLicenses.setOnClickListener {
-            val webView = android.webkit.WebView(this)
-            webView.loadUrl("file:///android_asset/open_source_licenses.html")
+
+            val webView =
+                android.webkit.WebView(this)
+
+            webView.loadUrl(
+                "file:///android_asset/open_source_licenses.html"
+            )
+
             android.app.AlertDialog.Builder(this)
                 .setTitle("Open source licenses")
                 .setView(webView)
-                .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+                .setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+                }
                 .show()
         }
 
+        /*
+         * MojAzad Telegram channel
+         */
         binding.layoutTgChannel.setOnClickListener {
-            Utils.openUri(this, AppConfig.TG_CHANNEL_URL)
+            Utils.openUri(
+                this,
+                "https://t.me/MojAzadNet"
+            )
         }
 
+        /*
+         * Privacy policy
+         */
         binding.layoutPrivacyPolicy.setOnClickListener {
-            Utils.openUri(this, AppConfig.APP_PRIVACY_POLICY)
+            Utils.openUri(
+                this,
+                AppConfig.APP_PRIVACY_POLICY
+            )
         }
 
+        /*
+         * Version
+         */
         "v${BuildConfig.VERSION_NAME} (${CoreNativeManager.getLibVersion()})".also {
             binding.tvVersion.text = it
         }
+
+        /*
+         * Application ID
+         */
         BuildConfig.APPLICATION_ID.also {
             binding.tvAppId.text = it
         }
