190ecab196b1c7e59781fb0c819ceb89aaf5e440
HamidOffline <44529274+HamidOffline@users.noreply.github.com>
2026-09-19 16:25:38 +0330

    Update MainActivity.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
@@ -55,7 +55,7 @@ class MainActivity : HelperBaseActivity(), NavigationView.OnNavigationItemSelect
 
     private lateinit var groupPagerAdapter: GroupPagerAdapter
     private var tabMediator: TabLayoutMediator? = null
-
+    private var isConnecting = false
     private val requestVpnPermission =
         registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
