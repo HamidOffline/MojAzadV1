71811f69f6ebd4cc7fede9cbdaaef5f987d6485c
MojAzad 🌊 <44529274+HamidOffline@users.noreply.github.com>
2026-09-21 18:54:15 +0330

    Update CheckUpdateResult.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/dto/CheckUpdateResult.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/dto/CheckUpdateResult.kt
@@ -3,8 +3,10 @@ package com.v2ray.ang.dto
 data class CheckUpdateResult(
     val hasUpdate: Boolean,
     val latestVersion: String? = null,
+    val releaseTag: String? = null,
     val releaseNotes: String? = null,
     val downloadUrl: String? = null,
+    val downloadFileName: String? = null,
     val error: String? = null,
     val isPreRelease: Boolean = false
 )
