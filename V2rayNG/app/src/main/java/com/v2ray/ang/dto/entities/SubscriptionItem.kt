89e2044033aae8c04c5a6c2a88f7c7446d78b4a8
MojAzad 🌊 <44529274+HamidOffline@users.noreply.github.com>
2026-09-22 04:26:49 +0330

    Update SubscriptionItem.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/dto/entities/SubscriptionItem.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/dto/entities/SubscriptionItem.kt
@@ -1,17 +1,63 @@
 package com.v2ray.ang.dto.entities
 
 data class SubscriptionItem(
-    var remarks: String = "",
-    var url: String = "",
-    var enabled: Boolean = true,
-    val addedTime: Long = System.currentTimeMillis(),
-    var lastUpdated: Long = -1,
-    var autoUpdate: Boolean = false,
-    var updateInterval: Long = 1440, // in minutes, default to 24 hours
-    var prevProfile: String? = null,
-    var nextProfile: String? = null,
-    var filter: String? = null,
-    var allowInsecureUrl: Boolean = false,
-    var userAgent: String? = null,
-)
 
+    var remarks: String = "",
+
+    var url: String = "",
+
+    var enabled: Boolean = true,
+
+    val addedTime: Long =
+        System.currentTimeMillis(),
+
+    var lastUpdated: Long = -1,
+
+    var autoUpdate: Boolean = false,
+
+    /*
+     * Update interval in minutes
+     */
+    var updateInterval: Long = 1440,
+
+    var prevProfile: String? = null,
+
+    var nextProfile: String? = null,
+
+    var filter: String? = null,
+
+    var allowInsecureUrl: Boolean = false,
+
+    var userAgent: String? = null,
+
+
+    /*
+     * =================================
+     * MojAzad Subscription Usage
+     * =================================
+     *
+     * Values are received from
+     * subscription-userinfo header
+     *
+     * upload:
+     * total uploaded traffic
+     *
+     * download:
+     * total downloaded traffic
+     *
+     * total:
+     * subscription traffic limit
+     *
+     * expire:
+     * unix timestamp
+     */
+
+    var uploadBytes: Long = 0L,
+
+    var downloadBytes: Long = 0L,
+
+    var totalBytes: Long = 0L,
+
+    var expireTime: Long = -1L,
+
+)
